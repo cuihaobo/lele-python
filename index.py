d0c77b5bcd9b07f9aea753c84fa1118359d3f598
le316Davis <105160372+le316Davis@users.noreply.github.com>
2022-09-19 20:14:25 +0800

    added classes

--- index.py
+++ index.py
@@ -1,1 +1,30 @@
-print("Hello World!")
+class Animal:
+    def breathe(self):
+        print("I am breathing")
+    def eat(self):
+        print("I am eating")
+    def sleep(self):
+        print("I am sleeping")
+    def breed(self):
+        print("I am breeding")
+
+class Fish(Animal):
+    def swim(self):
+        print("I am swimming")
+
+class Bird(Animal):
+    def fly(self):
+        print("I am flying")
+    def walk(self):
+        print("I am walking")
+
+class Person(Animal):
+    def walk(self):
+        print("I am walking")
+
+nemoFish = Fish()
+brendaBird = Bird()
+david = Person()
+david.breathe()
+brendaBird.breathe()
+nemoFish.breathe()

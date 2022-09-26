7ce618b4ce2e6d8837406f126b1fc154821b7875
le316Davis <105160372+le316Davis@users.noreply.github.com>
2022-09-26 19:48:04 +0800

    added string example from the book P75

--- index.py
+++ index.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+#String
+single_quote_string ='Hello I am a single quote string,my name is %s and I am %s years'
+double_quote_string="Hello I am a double quote string,my name is %s and I am %s years"
+triple_quote_multi_line_string = '''
+Hello I am a triple multi_line_string,my name is %s and I am %s years
+'''
+name = "Peppa pig"
+age = "8"
+#print(single_quote_string % (name,s))
+
+found_coins=20
+magic_coins=70
+stolen_coins=3
+coins = found_coins
+for week in range(1,53):
+    coins = coins + magic_coins - stolen_coins
+    print('Week %s = %s' % (week,coins))
